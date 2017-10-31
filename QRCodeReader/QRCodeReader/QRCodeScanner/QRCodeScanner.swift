d58c7f6476b8e9ae1f571fbaf62fda5518d8a788
DheerajRathore <rathoredheeraj7@gmail.com>
2017-10-31 22:31:50 +0530

    Camera usage message added in plist. QRCodeReader file added. Folder structure updated.

---
+++ QRCodeReader/QRCodeReader/QRCodeScanner/QRCodeScanner.swift
@@ -0,0 +1,13 @@
+//
+//  QRCodeScanner.swift
+//  QRCodeReader
+//
+//  Created by Dheeraj on 31/10/17.
+//  Copyright © 2017 Dheeraj Rathore. All rights reserved.
+//
+
+import Foundation
+
+class QRCodeScanner: NSObject {
+    
+}
